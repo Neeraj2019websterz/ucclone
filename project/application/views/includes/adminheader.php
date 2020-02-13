a50c6a2c4c2b1f29355d6fde1ecc8746c57a83b1
Neeraj <neerajthapa.websterz@gmail.com>
2020-02-13 19:14:09 +0530

    cutom css for admin

--- project/application/views/includes/adminheader.php
+++ project/application/views/includes/adminheader.php
@@ -6,8 +6,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Dashboard - SB Admin</title>
-        <link href="<?PHP echo base_url('assets/admin_assets/css/styles.css');?>" rel="stylesheet" />
+        <title>Dashboard - UC Clone</title>
+        <link href="<?PHP echo base_url('assets/admin_assets/css/styles.css'); ?>" rel="stylesheet" />
+        <link href="<?PHP echo base_url('assets/admin_assets/css/custom_style_admin.css'); ?>" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
     </head>
